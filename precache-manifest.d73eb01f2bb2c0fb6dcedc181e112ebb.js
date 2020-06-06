a417d8616d38c313bfa6515b6570430766518f50
zombienerx <nermin.sehic@gmail.com>
2020-06-07 01:05:10 +0200

    Updates

---
+++ precache-manifest.d73eb01f2bb2c0fb6dcedc181e112ebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bef56a7f886485848879e35a781f562",
+    "url": "/react-mon--rol/index.html"
+  },
+  {
+    "revision": "0a8e0485968cfae4311e",
+    "url": "/react-mon--rol/static/css/main.6c01dd76.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-mon--rol/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-mon--rol/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a8e0485968cfae4311e",
+    "url": "/react-mon--rol/static/js/main.5ec39d40.chunk.js"
+  },
+  {
+    "revision": "49da98fe2cbf79e42756",
+    "url": "/react-mon--rol/static/js/runtime-main.995b5c96.js"
+  }
+]);
